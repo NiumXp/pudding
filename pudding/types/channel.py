02887c0c8b48e6c16c34c1548a56b2de149724b0
NiumXp <formigacomcaimbra@gmail.com>
2021-09-06 23:58:39 -0300

    add some types in Invite

--- pudding/types/channel.py
+++ pudding/types/channel.py
@@ -3,7 +3,20 @@ __all__ = (
 )
 
 import typing as t
+from .snowflake import Snowflake
+
+class _RawChannel(t.TypedDict):
+    id: Snowflake
+    name: str
 
 
-class Channel(t.TypedDict, total=False):
-    ...
+class _RawGuildChannel(_RawChannel):
+    pass
+
+
+class PartialChannel:
+    pass
+
+
+class Channel(_RawChannel):
+    pass
--- pudding/types/invite.py
+++ pudding/types/invite.py
@@ -1,2 +1,29 @@
-class Invite:
-    ...
+import typing as t
+
+from .user import User
+from .guild import PartialGuild
+from .channel import PartialChannel
+from .application import PartialApplication
+
+TargetType = t.Literal[1, 2]
+
+
+class InviteMetadata(t.TypedDict):
+    uses: int
+    max_uses: int
+    max_age: int
+    temporary: bool
+    created_at: str
+
+class Invite(t.TypedDict, total=False):
+    code: str
+    guild: t.Optional[PartialGuild]
+    channel: PartialChannel
+    inviter: t.Optional[User]
+    target_type: t.Optional[TargetType]
+    target_user: t.Optional[User]
+    target_application: t.Optional[PartialApplication]
+    approximate_presence_count: t.Optional[int]
+    approximate_member_count: t.Optional[int]
+    expires_at: t.Optional[str]
+    # stage_instance: t.Optional[]
