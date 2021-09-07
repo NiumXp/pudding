dbc02f91b80fff636988b25f5f22b1d387d605de
= <=>
2021-09-07 10:30:12 -0300

    ADDED a get_guild method

--- pudding/http.py
+++ pudding/http.py
@@ -183,6 +183,13 @@ class DiscordHTTPClient:
         return await self.request(r, reason=reason)
 
     # Guild
+    
+    async def get_guild(self,
+        guild_id: types.Snowflake
+    ) -> t.Dict[str, t.Union[str, int, t.List[t.Any]]]:
+        r = Route("GET", "/guilds/{guild_id}", guild_id=guild_id)
+
+        return await self.request(r)
 
     # Guild Template
 
