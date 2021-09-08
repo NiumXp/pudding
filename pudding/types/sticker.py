94672d2f4681cca18a6821dc448a9279fd5f7209
NiumXp <formigacomcaimbra@gmail.com>
2021-09-08 12:34:32 -0300

    add PartialSticker and StickerPack

--- pudding/types/sticker.py
+++ pudding/types/sticker.py
@@ -7,6 +7,12 @@ StickerType = t.Literal[1, 2]
 StickerFormatType = t.Literal[1, 2, 3]
 
 
+class PartialSticker(t.TypedDict):
+    id: Snowflake
+    name: str
+    format_type: StickerFormatType
+
+
 class _OptionalSticker(t.TypedDict, total=False):
     pack_id: t.Optional[Snowflake]
     available: t.Optional[bool]
@@ -15,11 +21,18 @@ class _OptionalSticker(t.TypedDict, total=False):
     sort_value: t.Optional[int]
 
 
-class Sticker(_OptionalSticker):
-    id: Snowflake
-    name: str
+class Sticker(_OptionalSticker, PartialSticker):
     description: t.Optional[str]
     tags: str
     asset: t.Literal['']  # deprecated
     type: StickerType
-    format_type: StickerFormatType
+
+
+class StickerPack(t.TypedDict):
+    id: Snowflake
+    stickers: t.List[Sticker]
+    name: str
+    sku_id: Snowflake
+    cover_sticker_id: t.Optional[Snowflake]
+    description: str
+    banner_asset_id: Snowflake
